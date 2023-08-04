d6baf55b76f4923e3df89f972c549a234e150e3a
rulidh <rulidh02@gmail.com>
2023-08-04 11:05:32 +0700

    Add Readme.MD

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,17 +31,12 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script src="/js/CardCarousel.js"></script>
 <script>
-    window.addEventListener('unload', function(e){
-        $.ajax({
-            url: "{{ route('ip.offline') }}",
-            type: 'get',
-            success: console.log(e)
-        });
-    });
-    $(window).unload(function(){
-        $.ajax({
-            url: "{{ route('ip.offline') }}",
-            type: 'get',
+    $(document).ready(function(){
+        $(window).unload(function(){
+            $.ajax({
+                url: "{{ route('ip.offline') }}",
+                type: 'get',
+            });
         });
     });
 </script>
